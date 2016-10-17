8c4f836aec55c94c2b2510d0b3e7ec9b29da143a
anyaliang <603520028@qq.com>
2016-10-17 16:25:07 +0800

    Update and rename ex6.md to ex9.py

---
+++ ex9.py
@@ -0,0 +1,18 @@
+# * Why do the \n newlines not work when I use %r? </br>
+# That's how %r formatting works; it prints it the way you wrote it (or close to it). It's the "raw" format for debugging.
+
+# Here's some new strange stuff, remember type it  exactly.
+
+days = 'Mon Tue Wed Thu Fri Sat Sun'
+months = 'Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug'
+
+print('Here are the days:',days)
+print('Here are the months:',months)
+
+print('''
+There\'s something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+''')
+
