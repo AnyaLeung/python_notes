b96509ad43d7155f7e9e287738f30750db504cf8
anyaliang <603520028@qq.com>
2016-10-19 20:37:02 +0800

    Create EX13.py

---
+++ EX13.py
@@ -0,0 +1,12 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print('The script is called:', script)
+print('Your first variable is:', first)
+print('Your second variable is:', second)
+print('Your third variable is：', third)
+
+# "import" is how you add features to your script from the Python feature set.
+# Python asks you to say what you plan to use.
+# The `argv` is the "argument variable"
