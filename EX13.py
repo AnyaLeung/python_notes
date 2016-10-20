78f492d03b83071d1aa80dc9a192ce19b6ea6e96
anyaliang <603520028@qq.com>
2016-10-20 09:42:40 +0800

    Update EX13.py

--- EX13.py
+++ EX13.py
@@ -1,3 +1,6 @@
+# script being another name for your .py files
+# MODULE: these little things you import to make your Python program do more | IMPORT MODULES
+
 from sys import argv
 
 script, first, second, third = argv
@@ -7,6 +10,9 @@ print('Your first variable is:', first)
 print('Your second variable is:', second)
 print('Your third variable is：', third)
 
-# "import" is how you add features to your script from the Python feature set.
-# Python asks you to say what you plan to use.
 # The `argv` is the "argument variable"
+
+# What's the difference between argv and raw_input()?
+# **where** the user is required to give input. 
+# If they give your script inputs on the **command line**, then you use argv.
+# If you want them to input using the keyboard **while the script is running**, then use input().
