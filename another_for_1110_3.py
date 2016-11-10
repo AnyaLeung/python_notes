857b0ce1e94f32408f79820826fb0d614030f7a7
anyaliang <603520028@qq.com>
2016-11-11 02:05:09 +0800

    create

---
+++ another_for_1110_3.py
@@ -0,0 +1,12 @@
+num = int(input('enter an int:'))
+m = 1
+
+while m <= num:
+    if m % 2 == 1:
+        print('*', end = "")
+
+    if m % 2 == 0:
+        print('+', end = "")
+    m += 1
+
+print('\n')
